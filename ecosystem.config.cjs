257a2b68f33c937f9444726e9b36efe9329d6b80
samuelyuma <samuelyuma.117@gmail.com>
2025-11-19 09:43:43 +0700

    fix: shorten pm2 process name

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: 'fp-pemweb-backend-development',
+      name: 'fp-be-development',
       script: 'dotenv -e .env.development -- ./server',
       cdw: '/root/FP-PemrogramanWebsite-BE-2025/development',
       env_file: '.env.development',
       shutdown_with_message: true
     },
     {
-      name: 'fp-pemweb-backend-production',
+      name: 'fp-be-production',
       script: 'dotenv -e .env.production -- ./server',
       cdw: '/root/FP-PemrogramanWebsite-BE-2025/production',
       env_file: '.env.production',
